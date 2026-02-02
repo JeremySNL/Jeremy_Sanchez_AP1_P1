abe34b4e0304df4d36dc9af0a7f03bcf8f541025
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-02-02 19:52:06 -0400

    Agrega el contexto de la DB

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Jeremy_Sanchez_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jeremy_Sanchez_AP1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<ViajesEspaciales> ViajesEspaciales { get; set; }
+}
+
