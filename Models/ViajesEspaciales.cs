3544d98bf54e74389b653b3326f3a75de129189d
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-02-02 19:45:52 -0400

    Agrega el modelo ViajesEspaciales

---
+++ Models/ViajesEspaciales.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jeremy_Sanchez_AP1_P1.Models;
+
+public class ViajesEspaciales
+{
+    [Key]
+    public int ViajeId { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "La descripcion es obligatoria")]
+    [StringLength(200, ErrorMessage = "La descripcion tiene un maximo de 200 ")]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "El costo es obligatorio")]
+    public double Costo { get; set; }
+}
