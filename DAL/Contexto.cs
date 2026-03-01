8b95d02bcd54b9a66bd3f39fea5fd03fc2a9d740
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-03-01 11:09:06 -0400

    Agrega los DbSet de los modelos creados en el Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,18 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+    public DbSet<DetallesEntradas> DetallesEntradas { get; set; }
+    public DbSet<TiposHuacales> TiposHuacales { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {   
+        modelBuilder.Entity<TiposHuacales>().HasData(new List<TiposHuacales>()
+        {
+            new TiposHuacales() { TipoId = 1, Descripcion = "Huacales Verdes" },
+            new TiposHuacales() { TipoId = 2, Descripcion = "Huacales Rojos" },
+            new TiposHuacales() { TipoId = 3, Descripcion = "Huacales Amarillos" }
+        });
+        base.OnModelCreating(modelBuilder);
+    }
 }
 
