4d456d0a849488ca6b298c192e25754b795ec88d
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-03-01 11:10:06 -0400

    Agrega el Service de TiposHuacales

---
+++ Services/TiposHuacalesServices.cs
@@ -0,0 +1,14 @@
+﻿using Jeremy_Sanchez_AP1_P1.DAL;
+using Jeremy_Sanchez_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jeremy_Sanchez_AP1_P1.Services;
+
+public class TiposHuacalesServices(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<TiposHuacales>> Listar()
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.TiposHuacales.Where(t => t.TipoId > 0).ToListAsync();
+    }
+}
