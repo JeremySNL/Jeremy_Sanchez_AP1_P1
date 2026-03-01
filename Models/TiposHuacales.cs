b6ea6cab39550faf53d8d2bd0b3d0089ce8346c8
Jeremy Sanchez Cabrera <jeremy_sanchez@ucne.edu.do>
2026-03-01 11:06:40 -0400

    Agrega el modelo de TiposHuacales

---
+++ Models/TiposHuacales.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Jeremy_Sanchez_AP1_P1.Models;
+
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "La descripcion es obligatoria")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue)]
+    public int Existencia { get; set; }
+
+    [ForeignKey("TipoId")]
+    public ICollection<DetallesEntradas> DetallesEntradas { get; set; } = new List<DetallesEntradas>();
+}
